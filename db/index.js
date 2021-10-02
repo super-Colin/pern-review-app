1f3909242652c524c197ba90de4ccfc1f4542708
superColin <supercolin.dev@gmail.com>
2021-10-02 19:20:49 -0400

    trying to enable sll for pg connection

--- db/index.js
+++ db/index.js
@@ -1,9 +1,11 @@
 const { Pool } = require("pg");
+pg.defaults.ssl = true;
 
 const connectionString = process.env.DATABASE_URL
 
 const pool = new Pool({
   connectionString,
+  // ssl: true
 });
 module.exports = {
   query: (text, params) => pool.query(text, params),

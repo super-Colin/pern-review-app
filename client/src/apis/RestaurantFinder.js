b9107d1ade837a265d6bbb10f9bcbac85d0f1312
superColin <supercolin.dev@gmail.com>
2021-10-02 19:48:54 -0400

    add ssl to api connection

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 const apiPort = process.env.PORT || 3001;
 
 export default axios.create({
-  baseURL: `http://${window.location.hostname}:${apiPort}/api/v1/restaurants`,
+  baseURL: `https://${window.location.hostname}:${apiPort}/api/v1/restaurants`,
 });

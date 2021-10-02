ccdb3d5e3d6ff55175eff82f89a03378d1096d3a
superColin <supercolin.dev@gmail.com>
2021-10-02 19:56:47 -0400

    remove port api connection

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const apiPort = process.env.PORT || 3001;
+// const apiPort = process.env.PORT || 3001;
 
 export default axios.create({
-  baseURL: `https://${window.location.hostname}:${apiPort}/api/v1/restaurants`,
+  baseURL: `https://${window.location.hostname}/api/v1/restaurants`,
 });
